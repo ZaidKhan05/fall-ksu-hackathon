7938c394e7acce3033e9a900be53f1de161eacb4
ZaidKhan05 <zaidkhan5213@gmail.com>
2023-09-14 15:53:42 -0400

    e

---
+++ public class Main {.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+    
+
+
+    }
+}
+
+
+/*
+Challenge
+Given today’s rising transportation challenges with pandemics, inflation, fuel costs, labor
+shortage, customer expectations and environmental impact awareness, the need to fully utilize
+shipment capacity is becoming a strategic necessity.
+Modern supply chains must maximize whatever shipping mode is available to meet the
+demands of their customer while minimizing transportation costs, but much of the industry
+does not utilize efficient planning tools which leaves their shipments only partially full. This is a
+problem called "paying to ship air".
+Your Mission (…should you choose to accept it…) is to develop a model that will optimize usage
+of the available space of a container given the cubic space and the items needing to be
+shipped.
+Examples of a Container Space To Optimize
+Box Truck
+24ft Length, 8ft High and 8ft wide. Max Weight 26,000 pounds.
+Shipping Container
+40ft Length, 8’6” high and 8ft wide. Max Weight 59,200 Pounds (also 10ft & 20ft Length).
+Storage Unit
+20ft Length, 8ft high and 10ft wide.
+Pallets (Fit inside containers)
+48” length, 5ft high and 40” wide. Max Weight 4,600 pounds.
+Sample Products
+Filters, Brake Pads, Windshield Wipers, Oil, Car Batteries, Rotors, Fuzzy Dice, etc.
+Other Variables to Consider
+Pack Size, Max Stacking Height, Handling, Max Weight Capacity, Hazardous Materials, etc.
+Bonus (5%)
+To avoid ordering too much inventory, companies look at the “Lead Time” and “Sales Forecast”
+to decide how much of the product to order for a specific period. Given the desire to also
+Optimize how much inventory you order, what strategies would you build into your model to
+avoid ordering too much product when trying to maximize space utilization.
+ */
